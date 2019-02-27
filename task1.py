54c9838fc6f59b19bfbe8fbe1959756d551afcbf
tatsi94 <tusov94@gmail.com>
2019-02-27 19:44:24 +0300

    1_solved(M+D), valid

---
+++ task1.py
@@ -0,0 +1,37 @@
+S = [(-1, 0.1), (2, 0.2), (5, 0.3), (10, 0.3), (20, 0.1)]
+
+def valid(data):
+    res = 0
+    for _, j in data:
+        res += j
+
+    return abs(res - 1) < 0.0001
+
+
+def m(data):
+    res = 0
+    for i, j in data:
+        res += i * j
+
+    return res
+
+
+def d(data):
+    res = 0
+    ozh = m(data)
+    for i, j in data:
+        res += j * ((i - ozh) ** 2)
+
+    return res
+
+
+def main():
+    if not valid(S):
+        print("Aiaiai")
+        return
+
+    print("M = %s" % m(S))
+    print("D = %s" % d(S))
+
+
+main()
