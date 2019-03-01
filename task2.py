54cadd423f9e1640e0539e42ba38d8fac5e67827
Tatsi94 <48032676+Tatsi94@users.noreply.github.com>
2019-03-01 23:36:37 +0300

    ver2

--- task2.py
+++ task2.py
@@ -1,13 +1,13 @@
 vector1 = [2, 6, -7, 9]
 vector2 = [7, 80, 32, -1]
 
-mult = zip(vector1,vector2)
 
-
-def scalar_product(data):
+def scalar_product(a, b):
+    multiplication = zip(vector1, vector2)
     res = 0
-    for i, j in mult:
+    for i, j in multiplication:
         res += i * j
     return res
 
-print("Скалярное произведение векторов составляет % s" % scalar_product(mult))
+
+print("Скалярное произведение векторов составляет % s" % scalar_product(vector1, vector2))
