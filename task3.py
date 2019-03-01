d25adc495e25cb6a1f2dddcc2ad24b12f4adfd9e
Tatsi94 <48032676+Tatsi94@users.noreply.github.com>
2019-03-01 23:30:01 +0300

    ver2

--- task3.py
+++ task3.py
@@ -2,4 +2,4 @@ vector = [2, 5, 8]
 
 vector_length = (sum(i**2 for i in vector)**0.5)
 
-print("Длина вектора составляет % s" % vector_length)
+print("Длина вектора составляет % s" % vector_length) 
