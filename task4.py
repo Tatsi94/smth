1548da937fd18fb9d2f2a17dc4ef3d31addb56e4
Tatsi94 <48032676+Tatsi94@users.noreply.github.com>
2019-03-02 01:34:02 +0300

    ver3

--- task4.py
+++ task4.py
@@ -6,13 +6,15 @@ k = n - m
 
 
 def factorial(data):
-    if data == 0:
-        return 1
-    else:
-        return data * factorial(data - 1)
+    res = 1
+    for data in range(1, data-1):
+        res = data * (factorial(data - 1))
+    return res
+
 
 def probability(data):
     res = (factorial(n) / (factorial(m) * factorial(k))) * (((7 - s) / 6)**m) * (1 - ((7 - s) / 6))**k
     return res
+
 
 print("Вероятность составляет % s" % probability(factorial))
